c8b01f04c14160088e0db91a321896a6977165b3
Sakshi Bobade <bobadesakshi21@gmail.com>
2022-01-04 18:05:09 +0530

    maintaining single state for 3 different inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,51 @@
+import { useState } from 'react'
+
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+//   const [enteredTitle, setEnteredTitle] = useState('');
+
+//   const titleChangeHandler = (event) => {
+//     setEnteredTitle(event.target.value);
+//   }     
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''    
+  })
+
+  const titleChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value }
+    });
+  }
+
+  const amountChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value }
+    });
+  }
+
+  const dateChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value }
+    });
+  }
+
   return <form>
     <div className='new-expense__controls'>
       <div className='new-expense__controls'>
         <label>Title</label>
-        <input type='text'></input>
+        <input type='text' onChange={titleChangeHandler}></input>
       </div>
       <div className='new-expense__controls'>
         <label>Amount</label>
-        <input type='number' min='0.01' step='0.01 '></input>
+        <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}></input>
       </div>
       <div className='new-expense__controls'>
         <label>Date</label>
-        <input type='date' min='2021-02-01' max='2022-02-01'></input>
+        <input type='date' min='2021-02-01' max='2022-02-01' onChange={dateChangeHandler}></input>
       </div>
     </div>
     <div className='new-expense__actions'>
